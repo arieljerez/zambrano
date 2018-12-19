763011562c7a7f79e25765f979dbd42e6930868b
arieljerez <arieljerez@gmail.com>
2018-12-19 09:59:06 -0300

    efector, panel de control

--- app/Http/Controllers/EfectorController.php
+++ app/Http/Controllers/EfectorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Caso;
 
 class EfectorController extends Controller
 {
@@ -17,6 +18,11 @@ class EfectorController extends Controller
 
     public function home()
     {
-      return view('efector.home');
+      $aprobados = Caso::where('estado','=','aprobado')->count();
+      $rechazados = Caso::where('estado','=','rechazado')->count();
+      $pendientes_aprobacion = Caso::where('estado','=','pendiente_aprobacion')->count();
+      $pendientes_formulario = Caso::where('estado','=','pendiente_formulario')->count();
+      $vencidos = Caso::where('estado','=','vencido')->count();
+      return view('efector.home',compact('aprobados','rechazados','pendientes_aprobacion','pendientes_formulario','vencidos'));
     }
 }
--- resources/views/efector/home.blade.php
+++ resources/views/efector/home.blade.php
@@ -2,51 +2,66 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-10">
-                    <!-- -->
-            <div class="row">
-                  <div class="col-sm-6">
-                    <div class="card border-danger" style="max-width: 18rem;">
-                      <div class="card-body">
-                        <h4 class="card-title">Rechazados</h4>
-                        <p class="card-text"><h1>20</h1></p>
-                        <a href="{{ route('casos.rechazados') }}" class="btn btn-primary">Ver</a>
-                      </div>
-                    </div>
+  <div class="row justify-content-center">
+      <div class="col-md-10">
+      <div class="card">
+          <div class="card-header">Panel de Control</div>
+          <div class="card-body">
+          <div class="row">
+              <div class="col-md-4">
+                <div class="card border-danger" style="max-width: 18rem;">
+                  <div class="card-body">
+                    <h4 class="card-title">Rechazados</h4>
+                    <p class="card-text"><h1>{{ $rechazados }}</h1></p>
+                    <a href="{{ route('casos.rechazados') }}" class="btn btn-primary">Ver</a>
                   </div>
-                    <div class="col-sm-6">
-                      <div class="card border-warning mb-3" style="max-width: 18rem;">
-                        <div class="card-body">
-                          <h4 class="card-title">Pendientes Aprobación</h4>
-                            <p class="card-text"><h1>45</h1></p>
-                          <a href="#" class="btn btn-primary">Ver</a>
-                        </div>
-                      </div>
-                    </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card border-danger mb-3" style="max-width: 18rem;">
+                  <div class="card-body">
+                    <h4 class="card-title">Vencidos</h4>
+                      <p class="card-text"><h1>{{ $vencidos }}</h1></p>
+                    <a href="{{ route('casos.vencidos') }}" class="btn btn-primary">Ver</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card border-warning mb-3" style="max-width: 18rem;">
+                  <div class="card-body">
+                    <h4 class="card-title">Pendientes Aprobación</h4>
+                      <p class="card-text"><h1>{{ $pendientes_aprobacion }}</h1></p>
+                    <a href="{{ route('casos.pendientes-aprobacion') }}" class="btn btn-primary">Ver</a>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class="row">
-                  <div class="col-sm-6">
+                  <div class="col-sm-4">
                     <div class="card border-success" style="max-width: 18rem;">
                       <div class="card-body">
                         <h4 class="card-title">Aprobados</h4>
-                        <p class="card-text"><h1>260</h1></p>
+                        <p class="card-text"><h1>{{$aprobados}}</h1></p>
                         <a href="{{ route('casos.aprobados') }}" class="btn btn-primary">Ver</a>
                       </div>
                     </div>
                   </div>
-                    <div class="col-sm-6">
-                      <div class="card border-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-body">
-                          <h4 class="card-title">Pendientes Formulario</h4>
-                            <p class="card-text"><h1>75</h1></p>
-                          <a href="#" class="btn btn-primary">IR</a>
-                        </div>
+                  <div class="col-sm-4">
+
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="card border-primary mb-3" style="max-width: 18rem;">
+                      <div class="card-body">
+                        <h4 class="card-title">Pendientes Formulario</h4>
+                          <p class="card-text"><h1>{{ $pendientes_formulario }}</h1></p>
+                        <a href="{{ route('casos.pendientes-formulario') }}" class="btn btn-primary">Ver</a>
                       </div>
                     </div>
+                  </div>
             </div>
-                    <!-- -->
+          </div>
         </div>
+      </div>
     </div>
 </div>
 @endsection
