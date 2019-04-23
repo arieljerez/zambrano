5c60d5d6035db53a0ff69757517a3fc9f62a2223
Cobierto <cobierto@cobierto.tech>
2019-04-22 21:53:29 -0300

    bitacora usuario

--- resources/views/bitacora/table.blade.php
+++ resources/views/bitacora/table.blade.php
@@ -43,7 +43,7 @@
               <td>{{ $registro->descripcion }}</td>
               <td>
 
-                {{ $registro->usuario_tabla }} /
+                {{ $registro->usuario_tabla }} / {{ $registro->usuario->usuario }}
               </td>
           </tr>
         @endforeach
